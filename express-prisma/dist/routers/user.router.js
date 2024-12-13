110af1a6044c4da3f2d929960d8dbff80c230378
mirzaali45 <mirzaaliyusuf45@gmail.com>
2024-12-13 11:10:43 +0700

    update vercel

---
+++ express-prisma/dist/routers/user.router.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserRouter = void 0;
+const express_1 = require("express");
+const user_controller_1 = require("../controllers/user.controller");
+const verify_1 = require("../middlewares/verify");
+class UserRouter {
+    constructor() {
+        this.userController = new user_controller_1.UserController();
+        this.router = (0, express_1.Router)();
+        this.initializeRoutes();
+    }
+    initializeRoutes() {
+        this.router.get("/", verify_1.verifyToken, verify_1.checkAdmin, this.userController.getUsers);
+        this.router.post("/", this.userController.createUser);
+        this.router.get("/profile", verify_1.verifyToken, this.userController.getUserId);
+        this.router.patch("/:id", this.userController.editUser);
+        this.router.delete("/:id", this.userController.deleteUser);
+    }
+    getRouter() {
+        return this.router;
+    }
+}
+exports.UserRouter = UserRouter;
